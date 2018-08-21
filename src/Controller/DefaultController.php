1d35cd1be8850e5ecb19f797cfa7fb60103e6eaa
john <john@3works.gr>
2018-08-21 17:13:33 +0300

    enable instagram after upgrading php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,13 +10,12 @@ class DefaultController extends MainController
     {
         // Load categories from XML format to array
 //        $ctgEntity = $prCategories->InitializeCategories($categoriesXML, $session->get("authID"));
-
 //        $latest = $pr->getItems(-1, $session->get("authID"));
 
         // Create a new instagram instance.
-//        $instagram = new Instagram('2209588506.1677ed0.361223b4d3a547eebd1ad92202375d17');
+        $instagram = new Instagram('2209588506.1677ed0.361223b4d3a547eebd1ad92202375d17');
         // Fetch recent user media items.
-//        $this->instagramfeed = $instagram->media();
+        $this->instagramfeed = $instagram->media();
 
         return $this->render('layout.html.twig', [
             'categories' => $this->categories,
@@ -27,7 +26,7 @@ class DefaultController extends MainController
             'totalCartItems' => $this->totalCartItems,
             'loggedUser' => $this->loggedUser,
             'loggedName' => $this->loggedName,
-//            'instagramfeed' => $this->instagramfeed
+            'instagramfeed' => $this->instagramfeed
 //            'latest' => $latest
         ]);
     }
@@ -47,14 +46,14 @@ class DefaultController extends MainController
         ]);
     }
 
-    protected function instragam() {
-        // use this instagram access token generator http://instagram.pixelunion.net/
-        $access_token="2209588506.1677ed0.361223b4d3a547eebd1ad92202375d17";
-        $photo_count=9;
-
-        $json_link="https://api.instagram.com/v1/users/self/media/recent/?";
-        $json_link.="access_token={$access_token}&count={$photo_count}";
-        $json = file_get_contents($json_link);
-        $obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
-    }
+//    protected function instragam() {
+//        // use this instagram access token generator http://instagram.pixelunion.net/
+//        $access_token="2209588506.1677ed0.361223b4d3a547eebd1ad92202375d17";
+//        $photo_count=9;
+//
+//        $json_link="https://api.instagram.com/v1/users/self/media/recent/?";
+//        $json_link.="access_token={$access_token}&count={$photo_count}";
+//        $json = file_get_contents($json_link);
+//        $obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
+//    }
 }
