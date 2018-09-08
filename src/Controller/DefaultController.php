47834ac59d2d31ebd3ccb5f163a520d8c4a9c6dd
George Pittas <sattip@hotmail.com>
2018-09-08 12:03:50 +0300

    update branch

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Vinkla\Instagram\Instagram;
+use \Facebook;
 
 class DefaultController extends MainController
 {
@@ -14,8 +15,23 @@ class DefaultController extends MainController
 
         // Create a new instagram instance.
         $instagram = new Instagram('2209588506.1677ed0.361223b4d3a547eebd1ad92202375d17');
+
+        //fb page token
+        //EAAImVBEgHtQBAFhb99ycIu6WWZAOdOO3lb0M4M9q4aFOoSCZA4G2fd7W9LpsBZAoCELeykpMST4ZAOmVhygKT13rGRoMd4RXL1lqXGbzds0U1ZB3LTqhuRBMkb3r1pG6lLsaSAwHdMTXTmZB8u0KiKldQmo30Vy3VlJooKKK9agViGBc8zMi8nLW0KKZA9zXCpbfZCcV9sCVgeP7XpZCyZABrC
+
+
         // Fetch recent user media items.
         $this->instagramfeed = $instagram->media();
+
+        $fb = new \Facebook\Facebook([
+          'app_id' => '605092459847380',
+          'app_secret' => '09f4a59ad57726736664a92d7059025f',
+          'default_graph_version' => 'v3.0',
+          'default_access_token' => 'EAAImVBEgHtQBAFhb99ycIu6WWZAOdOO3lb0M4M9q4aFOoSCZA4G2fd7W9LpsBZAoCELeykpMST4ZAOmVhygKT13rGRoMd4RXL1lqXGbzds0U1ZB3LTqhuRBMkb3r1pG6lLsaSAwHdMTXTmZB8u0KiKldQmo30Vy3VlJooKKK9agViGBc8zMi8nLW0KKZA9zXCpbfZCcV9sCVgeP7XpZCyZABrC', // optional
+        ]);
+
+        $this->reviews = $fb->get('/292956054170320/ratings');
+        //print_r($response);
 
         return $this->render('layout.html.twig', [
             'categories' => $this->categories,
@@ -26,8 +42,9 @@ class DefaultController extends MainController
             'totalCartItems' => $this->totalCartItems,
             'loggedUser' => $this->loggedUser,
             'loggedName' => $this->loggedName,
-            'instagramfeed' => $this->instagramfeed
-//            'latest' => $latest
+            'latest' => $latest,
+            'instagramfeed' => $this->instagramfeed,
+            'reviews' => $this->reviews
         ]);
     }
 
