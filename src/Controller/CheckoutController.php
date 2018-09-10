7fc45c7cd598cf8f8345c3443e29206b49ada332
John Krav <john@3works.gr>
2018-09-10 21:55:45 +0300

    checkout divide guess from registered user

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -112,6 +112,113 @@ class CheckoutController extends MainController
                 'totalWishlistItems' => $this->totalWishlistItems,
                 'loggedUser' => $this->loggedUser,
                 'addresses' => $addresses,
+                'checkout' => $checkout,
+                'step1Form' => $step1Form->createView(),
+                'step2Form' => $step2Form->createView(),
+                'step3Form' => $step3Form->createView(),
+                'step4Form' => $step4Form->createView(),
+                'curStep' => $curStep
+            ]));
+        } catch (\Exception $e) {
+            $this->logger->error(__METHOD__ . ' -> {message}', ['message' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    public function guestCheckout(int $step, Request $request, CheckoutService $checkoutService, UserAccountService $userAccountService, EntityManagerInterface $em)
+    {
+        try {
+            $addresses = array();
+            if ($this->totalCartItems === 0) {
+                return $this->redirectToRoute('cart_view');
+            }
+            $curStep = ($request->request->get('currentStep')) ?: $step;
+            if ($this->session->has('curOrder') === false) {
+                $checkout = new Checkout();
+                if (null !== $this->loggedUser) {
+                    $checkoutService->getUserInfo($checkout);
+                }
+                $this->session->set('curOrder', $checkout);
+            } else {
+                $checkout = $this->session->get('curOrder');
+            }
+            if (null !== $this->loggedUser) {
+                $addresses = $userAccountService->getAddresses($this->loggedClientId);
+            }
+            dump($checkout);
+            $step1Form = $this->createForm(CheckoutStep1Type::class, $checkout, [
+                'loggedUser' => $this->loggedUser
+            ]);
+            $step1Form->handleRequest($request);
+            $step2Form = $this->createForm(CheckoutStep2Type::class, $checkout);
+            $step2Form->handleRequest($request);
+            $step3Form = $this->createForm(CheckoutStep3Type::class, $checkout);
+            $step3Form->handleRequest($request);
+            $step4Form = $this->createForm(CheckoutStep4Type::class, $checkout);
+            $step4Form->handleRequest($request);
+            if ($step1Form->isSubmitted() && $step1Form->isValid()) {
+                if (null !== $this->loggedUser) {
+
+                }
+                $curStep = 2;
+            } elseif ($step2Form->isSubmitted() && $step2Form->isValid()) {
+                $curStep = 3;
+            } elseif ($step3Form->isSubmitted() && $step3Form->isValid()) {
+                if ($step3Form->get('shippingType')->getData() === '1000') {
+                    $checkout->setShippingCost(2.00);
+                }
+                $curStep = 4;
+            }
+            $this->session->set('curOrder', $checkout);
+            if ($step4Form->isSubmitted() && $step4Form->isValid()) {
+                if ($step4Form->get('paymentType')->getData() === '1003') {
+                    $checkout->setAntikatavoliCost(1.50);
+                } else {
+                    $checkout->setAntikatavoliCost(0);
+                }
+                $curStep = 4;
+                $orderWebId = $em->getRepository(OrdersWebId::class)->find(1);
+                $checkout->setOrderNo($orderWebId->getOrderNumber() + 1);
+                $orderResponse = $checkoutService->submitOrder($checkout, $this->cartItems);
+                if ($orderResponse) {
+
+                    $this->addFlash(
+                        'success',
+                        'Η παραγγελία σας ολοκληρώθηκε με επιτυχία. Ένα αντίγραφο έχει αποσταλεί στο email σας ' . $checkout->getEmail() . '. Ευχαριστούμε που μας προτιμήσατε για τις αγορές σας!'
+                    );
+                    $checkoutService->sendOrderConfirmationEmail($checkout);
+//                    $checkoutService->emptyCart($this->cartItems, $em);
+                    $orderCompleted = true;
+                    // CLEAR curOrder SESSION
+                    return ($this->render('orders/order_completed.html.twig', [
+                        'categories' => $this->categories,
+                        'popular' => $this->popular,
+                        'featured' => $this->featured,
+                        'checkout' => $checkout,
+                        'loggedUser' => $this->loggedUser,
+                        'totalCartItems' => $this->totalCartItems,
+                        'totalWishlistItems' => $this->totalWishlistItems,
+                        'cartItems' => $this->cartItems,
+                        'orderCompleted' => $orderCompleted
+                    ]));
+                } else {
+                    $this->addFlash(
+                        'notice',
+                        'Ένα σφάλμα παρουσιάστηκε κατά την διαδικασία της παραγγελίας σας. Παρακαλούμε προσπαθήστε ξανά. Αν το πρόβλημα παραμείνει επικοινωνήστε μαζί μας!'
+                    );
+                    $orderCompleted = false;
+                }
+            }
+            dump($addresses);
+            return ($this->render('orders/checkout.html.twig', [
+                'categories' => $this->categories,
+                'popular' => $this->popular,
+                'featured' => $this->featured,
+                'cartItems' => $this->cartItems,
+                'totalCartItems' => $this->totalCartItems,
+                'totalWishlistItems' => $this->totalWishlistItems,
+                'loggedUser' => $this->loggedUser,
+                'addresses' => $addresses,
                 'step1Form' => $step1Form->createView(),
                 'step2Form' => $step2Form->createView(),
                 'step3Form' => $step3Form->createView(),
--- src/Form/Type/CheckoutStep1Type.php
+++ src/Form/Type/CheckoutStep1Type.php
@@ -6,10 +6,25 @@ use App\Entity\Checkout;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\{CheckboxType, EmailType, HiddenType, TextType};
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CheckoutStep1Type extends AbstractType
 {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * CheckoutStep2Type constructor.
+     * @param SessionInterface $session
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -22,6 +37,9 @@ class CheckoutStep1Type extends AbstractType
             ->add('newsletter', CheckboxType::class, array(
                 'required' => false,
             ));
+
+        $builder->addEventSubscriber(new CheckoutSubscriber($options, $this->session));
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
