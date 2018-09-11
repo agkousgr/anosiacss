efbc9240177f74e9d95c811f23c1861a5aa93351
john <john.kravaritis@enerdia.gr>
2018-09-11 14:19:55 +0300

    product view extra images

--- src/Migrations/Version20180909163529.php
+++ src/Migrations/Version20180909163529.php
@@ -15,7 +15,7 @@ final class Version20180909163529 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE product_views (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, views INT NOT NULL, UNIQUE INDEX UNIQ_B0ACE7DB4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+//        $this->addSql('CREATE TABLE product_views (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, views INT NOT NULL, UNIQUE INDEX UNIQ_B0ACE7DB4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE orders_web_id (id INT AUTO_INCREMENT NOT NULL, order_number INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
