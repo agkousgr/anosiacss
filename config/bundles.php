3dcc208665f7f00b1746d230576cb5bd10d33667
john <john.kravaritis@enerdia.gr>
2018-10-23 19:08:51 +0300

    frontpage changes

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-//    Beelab\PaypalBundle\BeelabPaypalBundle::class => ['all' => true],
 ];
